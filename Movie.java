724f5f4acebbcf872acc51680ceef26d7a72bf32
gabrielperson <gabrielperson.pm@gmail.com>
2019-11-10 18:04:33 -0300

    commit 8

--- Movie.java
+++ Movie.java
@@ -87,6 +87,26 @@ class Customer {
    //     return aRental.getCharge();
    // }
 
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+          Rental each = (Rental) rentals.nextElement();
+          result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
   public String statement() {
      double totalAmount = 0;
      int frequentRenterPoints = 0;
@@ -96,21 +116,15 @@ class Customer {
         //double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-        frequentRenterPoints  += each.getFrequentPoints();
-
-        // add bonus for a two day new release rental
-        if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            each.getDaysRented() > 1) frequentRenterPoints ++;
-
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += each.getCharge();;
+            String.valueOf(each.getCharge()) + "\n";
+
 
      }
      //add footer lines
-     result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
-     result += "You earned " + String.valueOf(frequentRenterPoints) +
+     result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+     result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
 }
