2c6461bb7d9ea17869fa9556def4f3e824d4ec8f
gabrielperson <gabrielperson.pm@gmail.com>
2019-11-10 18:43:48 -0300

    commit 10

--- Movie.java
+++ Movie.java
@@ -1,3 +1,23 @@
+import java.util.Vector;
+import java.util.Enumeration;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+class Main {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Testing.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
+
 public class Movie {
 
   public static final int  CHILDRENS = 2;
@@ -5,64 +25,67 @@ public class Movie {
   public static final int  NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
+  public Movie(String name, int priceCode) {
+      _title = name;
+      setPriceCode(priceCode);
   }
 
   public int getPriceCode() {
-      return _priceCode;
+      return _price.getPriceCode();
   }
 
   public void setPriceCode(int arg) {
-     _priceCode = arg;
+      switch (arg) {
+          case REGULAR:
+             _price = new RegularPrice();
+             break;
+          case CHILDRENS:
+             _price = new ChildrensPrice();
+             break;
+          case NEW_RELEASE:
+             _price = new NewReleasePrice();
+             break;
+          default:
+             throw new IllegalArgumentException("Incorrect Price Code");
+       }
   }
 
   public String getTitle (){
       return _title;
   };
+
+  double getCharge(int daysRented) {
+      return _price.getCharge(daysRented);
+  }
+
+  int getFrequentRenterPoints(int daysRented) {
+      return _price.getFrequentRenterPoints(daysRented);
+  }
 }
 
 class Rental {
     private Movie _movie;
     private int _daysRented;
 
-    int getFrequentPoints(){
-        if((getMovie.getPriceCode() == Movie.NEW_RELEASE) && getDaysRented > 1)
-            return 2;
-        else
-            return 1;
-    }
-
-    double getCharge(){
-        double result = 0;
-        switch (getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          result += 2;
-          if (getDaysRented() > 2)
-             result += (getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          result += getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          result += 1.5;
-          if (getDaysRented() > 3)
-             result += (getDaysRented() - 3) * 1.5;
-          break;
-        }
-        return result;
-    }
-
     public Rental(Movie movie, int daysRented) {
       _movie = movie;
       _daysRented = daysRented;
     }
+
+    int getFrequentRenterPoints(){
+        return _movie.getFrequentRenterPoints(_daysRented);
+    }
+
+    double getCharge(){
+        return _movie.getCharge(_daysRented);
+    }
+
     public int getDaysRented() {
       return _daysRented;
     }
+
     public Movie getMovie() {
       return _movie;
     }
@@ -83,9 +106,6 @@ class Customer {
       return _name;
    };
 
-   // public double amountFor(Rental aRental){
-   //     return aRental.getCharge();
-   // }
 
     private double getTotalCharge() {
         double result = 0;
@@ -146,4 +166,51 @@ class Customer {
      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
              " frequent renter points";
      return result;
+}
+
+abstract class Price {
+   abstract int getPriceCode();
+
+   abstract double getCharge(int daysRented);
+
+   int getFrequentRenterPoints(int daysRented) {
+       return 1;
+   }
+}
+
+class ChildrensPrice extends Price {
+   int getPriceCode() {
+       return Movie.CHILDRENS;
+   }
+   double getCharge(int daysRented) {
+     double result = 1.5;
+     if (daysRented > 3)
+         result += (daysRented - 3) * 1.5;
+     return result;
+   }
+}
+
+class NewReleasePrice extends Price {
+   int getPriceCode() {
+       return Movie.NEW_RELEASE;
+   }
+   double getCharge(int daysRented){
+       return daysRented * 3;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return (daysRented > 1) ? 2: 1;
+    }
+}
+
+class RegularPrice extends Price {
+   int getPriceCode() {
+       return Movie.REGULAR;
+   }
+   double getCharge(int daysRented) {
+       double result = 2;
+       if (daysRented > 2)
+           result += (daysRented - 2) * 1.5;
+       return result;
+   }
 }
